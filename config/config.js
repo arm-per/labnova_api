c543bb06dc75217e9a500a8f7c9718326bb70d0b
Armando Pérez <armper.95@gmail.com>
2022-01-07 01:13:09 -0600

    volvemos a postgres

--- config/config.js
+++ config/config.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 const config = {
   env:        process.env.NODE_ENV || 'dev',
   port:       process.env.PORT || '3000',
-  dbUser:     process.env.DB_USER || 'nova_admin',
-  dbPassword: process.env.DB_PASSWORD || '20_L4Bnova_22',
+  dbUser:     process.env.DB_USER || 'admin_nova',
+  dbPassword: process.env.DB_PASSWORD || 'L4Bnova',
   dbHost:     process.env.DB_HOST || 'localhost',
   dbName:     process.env.DB_NAME || 'nova_db',
-  dbPort:     process.env.DB_PORT || '3306',
+  dbPort:     process.env.DB_PORT || '5432',
 }
 
 
